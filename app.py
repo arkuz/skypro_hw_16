08bdc2f45e4b14a11464c85420004e5473c230a5
Artem Kuzmenko <arkuzka@mail.ru>
2022-08-02 12:11:57 +0300

    Вынес errorhandler в app и добавил роуты для удаления сущностей

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 import os.path
 import const
-from flask import Flask
+from flask import Flask, jsonify
 from blueprints.stock_market.models.stock_market import db
 from blueprints.stock_market.views import stock_market_blueprint
 
@@ -20,6 +20,15 @@ def create_app() -> Flask:
     app.register_blueprint(stock_market_blueprint)
 
     db.init_app(app)
+
+    @app.errorhandler(const.ErrorCode.ERROR_404['code'])
+    def error_page_404(e):
+        return jsonify(const.ErrorCode.ERROR_404)
+
+    @app.errorhandler(const.ErrorCode.ERROR_500['code'])
+    def error_page_500(e):
+        return jsonify(const.ErrorCode.ERROR_500)
+
     return app
 
 
--- blueprints/stock_market/views.py
+++ blueprints/stock_market/views.py
@@ -13,57 +13,71 @@ stock_market_blueprint = Blueprint('stock_market_blueprint', __name__)
 
 @stock_market_blueprint.route('/users/')
 def get_users():
-    logger.info('Обращение к "/users/"')
     users = [user.to_dict() for user in db.session.query(User).all()]
     return jsonify(users)
 
 
 @stock_market_blueprint.route('/users/<int:id>/')
 def get_user_by_id(id):
-    logger.info(f'Обращение к "/users/{id}/"')
     user = db.session.query(User).filter(User.id == id).first()
     if user:
         return jsonify(user.to_dict())
     return jsonify(const.ErrorMessage.EMPTY_RESULT)
 
 
+@stock_market_blueprint.route('/users/<int:id>/', methods=['DELETE'])
+def delete_user_by_id(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify(user.to_dict())
+    return jsonify(const.ErrorMessage.EMPTY_RESULT)
+
+
 @stock_market_blueprint.route('/orders/')
 def get_orders():
-    logger.info('Обращение к "/orders/"')
     orders = [order.to_dict() for order in db.session.query(Order).all()]
     return jsonify(orders)
 
 
 @stock_market_blueprint.route('/orders/<int:id>/')
 def get_order_by_id(id):
-    logger.info(f'Обращение к "/orders/{id}/"')
     order = db.session.query(Order).filter(Order.id == id).first()
     if order:
         return jsonify(order.to_dict())
     return jsonify(const.ErrorMessage.EMPTY_RESULT)
 
 
+@stock_market_blueprint.route('/orders/<int:id>/', methods=['DELETE'])
+def delete_order_by_id(id):
+    order = Order.query.get(id)
+    if order:
+        db.session.delete(order)
+        db.session.commit()
+        return jsonify(order.to_dict())
+    return jsonify(const.ErrorMessage.EMPTY_RESULT)
+
+
 @stock_market_blueprint.route('/offers/')
 def get_offers():
-    logger.info('Обращение к "/offers/"')
     offers = [offer.to_dict() for offer in db.session.query(Offer).all()]
     return jsonify(offers)
 
 
 @stock_market_blueprint.route('/offers/<int:id>/')
 def get_offers_by_id(id):
-    logger.info(f'Обращение к "/offers/{id}/"')
     offer = db.session.query(Offer).filter(Offer.id == id).first()
     if offer:
         return jsonify(offer.to_dict())
     return jsonify(const.ErrorMessage.EMPTY_RESULT)
 
 
-@stock_market_blueprint.errorhandler(const.ErrorCode.ERROR_404['code'])
-def error_page_404(e):
-    return jsonify(const.ErrorCode.ERROR_404)
-
-
-@stock_market_blueprint.errorhandler(const.ErrorCode.ERROR_500['code'])
-def error_page_500(e):
-    return jsonify(const.ErrorCode.ERROR_500)
+@stock_market_blueprint.route('/offers/<int:id>/', methods=['DELETE'])
+def delete_offer_by_id(id):
+    offer = Offer.query.get(id)
+    if offer:
+        db.session.delete(offer)
+        db.session.commit()
+        return jsonify(offer.to_dict())
+    return jsonify(const.ErrorMessage.EMPTY_RESULT)
